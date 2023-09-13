266706722113f67ea064d2179dc019fafe168a2d
Daniel Bünzli <daniel.buenzli@erratique.ch>
2023-09-13 23:12:28 +0200

    Track b0

--- B0.ml
+++ B0.ml
@@ -23,98 +23,94 @@ let vz_canvas = B0_ocaml.libname "vz.canvas"
 (* Libraries *)
 
 let evidence_lib =
-  let srcs = Fpath.[ `File (v "src/evidence.mli");
-                     `File (v "src/evidence.ml") ] in
-  let requires = [] in
-  let doc = "The evidence library" in
-  B0_ocaml.lib evidence ~doc ~srcs ~requires
+  let srcs = [`File ~/"src/evidence.mli"; `File ~/"src/evidence.ml"] in
+  B0_ocaml.lib evidence ~srcs
 
 let evidence_samples_lib =
-  let srcs = Fpath.[`Dir (v "samples");] in
+  let srcs = [`Dir ~/"samples"] in
   let requires = [evidence] in
-  let doc = "The evidence.samples library" in
-  B0_ocaml.lib evidence_samples ~doc ~srcs ~requires
+  B0_ocaml.lib evidence_samples ~requires ~srcs
 
 let vz_lib =
-  let srcs = Fpath.[ `File (v "src/vz.mli"); `File (v "src/vz.ml") ] in
+  let srcs = [ `File ~/"src/vz.mli"; `File ~/"src/vz.ml" ] in
   let requires = [gg; gg_kit; vg] in
-  B0_ocaml.lib vz ~doc:"The vz library" ~srcs ~requires
+  B0_ocaml.lib vz ~srcs ~requires
 
 let vz_plot_lib =
-  let srcs = Fpath.[`File (v "src/vz_plot.mli"); `File (v "src/vz_plot.ml")] in
+  let srcs = [`File ~/"src/vz_plot.mli"; `File ~/"src/vz_plot.ml"] in
   let requires = [gg; gg_kit; vg; vg_htmlc; vz; evidence] in
-  B0_ocaml.lib vz_plot ~doc:"The vz.plot library" ~srcs ~requires
+  B0_ocaml.lib vz_plot ~srcs ~requires
 
 let vz_canvas_lib =
-  let srcs = Fpath.[`File (v "src/vz_canvas.mli"); `File (v "src/vz_canvas.ml")] in
+  let srcs = [`File ~/"src/vz_canvas.mli"; `File ~/"src/vz_canvas.ml"] in
   let requires = [gg; gg_kit; vg; vz; brr] in
-  B0_ocaml.lib vz_canvas ~doc:"The vz.canvas library" ~srcs ~requires
+  B0_ocaml.lib vz_canvas ~srcs ~requires
 
 let vz_doc_lib =
-  let srcs = Fpath.[`File (v "src/vz_doc.mli"); `File (v "src/vz_doc.ml")] in
+  let srcs = [`File ~/"src/vz_doc.mli"; `File ~/"src/vz_doc.ml"] in
   let requires = [gg; vg; vg_htmlc; brr; note; note_brr] in
-  B0_ocaml.lib vz_doc ~doc:"The vz.doc library" ~srcs ~requires
+  B0_ocaml.lib vz_doc ~srcs ~requires
 
 (* Tests *)
 
 let test_exe ?(requires = []) src ~doc =
-  let src = Fpath.v src in
-  let srcs = Fpath.[`File src] in
-  let meta = B0_meta.(empty |> tag test) in
+  let srcs = [`File src] in
+  let meta = B0_meta.empty |> B0_meta.(tag test) in
   let requires = gg :: gg_kit :: vg :: vz :: requires in
-  B0_ocaml.exe (Fpath.basename ~no_ext:true src) ~srcs ~doc ~meta ~requires
+  B0_ocaml.exe (Fpath.basename ~no_ext:true src) ~meta ~requires ~srcs ~doc
 
-let test = test_exe "test/test.ml" ~doc:"Vz tests"
+let test = test_exe ~/"test/test.ml" ~doc:"Vz tests"
 
 let test_with_brr ?(requires = []) src ~doc =
-  let src = Fpath.v src in
-  let srcs = Fpath.[`File src] in
+  let srcs = [`File src] in
   let requires = gg :: gg_kit :: vg :: vz :: vg_htmlc :: brr :: requires in
   let assets_root = Fpath.v "test" in
   let meta =
-    let comp_mode = `Separate in
-    B0_jsoo.meta ~requires ~assets_root ~comp_mode ~source_map:(Some `Inline) ()
+    B0_meta.empty
+    |> B0_meta.add B0_jsoo.compilation_mode `Separate
+    |> B0_meta.add B0_jsoo.source_map (Some `Inline)
   in
-  B0_jsoo.web (Fpath.basename ~no_ext:true src) ~doc ~srcs ~meta
+  let name = Fpath.basename ~no_ext:true src in
+  B0_jsoo.web name ~srcs ~requires ~assets_root ~meta ~doc
 
 let test_iris =
   let requires = [evidence; evidence_samples] in
-  test_with_brr "test/test_iris.ml" ~requires ~doc:"Test iris"
+  test_with_brr ~/"test/test_iris.ml" ~requires ~doc:"Test iris"
 
 let test_dev =
   let requires = [evidence; vz_plot; evidence_samples] in
-  test_with_brr "test/test_dev.ml" ~requires ~doc:"Test dev"
+  test_with_brr ~/"test/test_dev.ml" ~requires ~doc:"Test dev"
 
 let test_isolines =
   let requires = [vz_canvas] in
-  test_with_brr "test/isolines.ml" ~requires ~doc:"Test isolines"
+  test_with_brr ~/"test/isolines.ml" ~requires ~doc:"Test isolines"
 
 let test_metaballs =
   let requires = [vz_canvas] in
-  test_with_brr "test/metaballs.ml" ~requires ~doc:"Test isolines"
+  test_with_brr ~/"test/metaballs.ml" ~requires ~doc:"Test isolines"
 
 let test_doc =
   let requires = [note; vz_doc] in
-  test_with_brr "test/test_doc.ml" ~requires ~doc:"Test doc"
+  test_with_brr ~/"test/test_doc.ml" ~requires ~doc:"Test doc"
 
 (* Packs *)
 
 let default =
   let meta =
-    let open B0_meta in
-    empty
-    |> tag B0_opam.tag
-    |> add authors ["The vz programmers"]
-    |> add maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
-    |> add homepage "https://erratique.ch/software/vz"
-    |> add online_doc "https://erratique.ch/software/vz/doc/"
-    |> add licenses ["ISC"; "Apache-2.0"]
-    |> add repo "git+https://erratique.ch/repos/vz.git"
-    |> add issues "https://github.com/dbuenzli/vz/issues"
-    |> add description_tags
+    B0_meta.empty
+    |> B0_meta.(add authors) ["The vz programmers"]
+    |> B0_meta.(add maintainers)
+       ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
+    |> B0_meta.(add homepage) "https://erratique.ch/software/vz"
+    |> B0_meta.(add online_doc) "https://erratique.ch/software/vz/doc/"
+    |> B0_meta.(add licenses) ["ISC"; "Apache-2.0"]
+    |> B0_meta.(add repo) "git+https://erratique.ch/repos/vz.git"
+    |> B0_meta.(add issues) "https://github.com/dbuenzli/vz/issues"
+    |> B0_meta.(add description_tags)
       ["graphics"; "visualization"; "declarative"; "graphics";
        "org:erratique"]
-    |> add B0_opam.Meta.depends
+    |> B0_meta.tag B0_opam.tag
+    |> B0_meta.add B0_opam.depends
       [ "ocaml", {|>= "4.14.0"|};
         "ocamlfind", {|build|};
         "ocamlbuild", {|build|};
@@ -122,10 +118,9 @@ let default =
         "gg", {|>= "1.0.0"|};
         "vg", {|>= "0.9.4"|};
         "brr", {|>= "0.0.6"|};
-        "note", {||};
-      ]
-    |> add B0_opam.Meta.build
+        "note", {||} ]
+    |> B0_meta.add B0_opam.build
       {|[["ocaml" "pkg/pkg.ml" "build" "--dev-pkg" "%{dev}%"]]|}
   in
-  B0_pack.v "default" ~doc:"vz package" ~meta ~locked:true @@
+  B0_pack.make "default" ~meta ~locked:true ~doc:"vz package" @@
   B0_unit.list ()
